23cb4ef9f6f514973ca4690771c599cfd082930d
David.Rodrigues <David.Rodrigues@DRODRIGUES-PC>
2022-05-19 09:33:15 +0100

    Add project files.

---
+++ DJ_SNAKE/DJ_SNAKE.cpp
@@ -0,0 +1,12 @@
+﻿// DJ_SNAKE.cpp : Defines the entry point for the application.
+//
+
+#include "DJ_SNAKE.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
